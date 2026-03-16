6c8d7e4cfa4b0e5e5b9571c55f1ce0e4e449da88
MadMartigan3 <gabrielmilan3@hotmail.com>
2026-03-16 11:15:32 +0100

    task 5

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,9 @@
 import sha1 from 'sha1';
+import pkg from 'mongodb';
+import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
+
+const { ObjectID } = pkg;
 
 class UsersController {
   static async postNew(req, res) {
@@ -20,6 +24,21 @@ class UsersController {
     });
 
     return res.status(201).json({ id: result.insertedId, email });
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) return res.status(401).json({ error: 'Unauthorized' });
+
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+    if (!userId) return res.status(401).json({ error: 'Unauthorized' });
+
+    const usersCollection = await dbClient.collection('users');
+    const user = await usersCollection.findOne({ _id: new ObjectID(userId) });
+    if (!user) return res.status(401).json({ error: 'Unauthorized' });
+
+    return res.status(200).json({ id: user._id, email: user.email });
   }
 }
 
