950f65e0e0e80648912c05d5e037d33ed4fb171b
MadMartigan3 <gabrielmilan3@hotmail.com>
2026-03-16 10:10:55 +0100

    projet redis task 0

---
+++ utils/redis.mjs
@@ -0,0 +1,40 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.connected = true;
+
+    this.client.on('error', (err) => {
+      console.error('Redis client error:', err);
+      this.connected = false;
+    });
+
+    this.client.on('connect', () => {
+      this.connected = true;
+    });
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    const setexAsync = promisify(this.client.setex).bind(this.client);
+    return setexAsync(key, duration, value);
+  }
+
+  async del(key) {
+    const delAsync = promisify(this.client.del).bind(this.client);
+    return delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
